75019ae34f660150a20942ba0699b4d3aa7e9c3f
MarsYoung <bigmazhiyu@gmail.com>
2015-04-17 17:56:08 +0800

    init project

---
+++ dubbo-demo-api/src/main/java/com/marsyoung/dubbo/api/App.java
@@ -0,0 +1,13 @@
+package com.marsyoung.dubbo.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ dubbo-demo-core/src/main/java/com/marsyoung/demo/core/App.java
@@ -0,0 +1,13 @@
+package com.marsyoung.demo.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
